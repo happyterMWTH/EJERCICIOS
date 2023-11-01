c5ab71bed79f0d3425c5a4e66a92ffff2f8b8fcd
happyterMWTH <62663250+happyterMWTH@users.noreply.github.com>
2023-11-01 12:52:20 -0500

    js

---
+++ assets/js/introduccion.js
@@ -0,0 +1,16 @@
+// scripts ECMaScripts isomorfic;
+// comentarios
+// variables
+
+let numero = 1;
+let nombre = 'Juanma';
+let miVar = true;
+let miArray = [1,2,3];
+let num1 = 2;
+let num2 = 3;
+let suma = num1 + num2;
+
+console.log(`la suma es ${suma}`);
+
+let num3 = parseInt(prompt("digite el primer número"));
+console.log(`${num3}`);
